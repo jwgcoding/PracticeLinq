0d361d52c8b7767351b1b8ffc7f7b21594ce5f44
Gary Li <garyli0802@yahoo.com>
2022-09-30 08:27:50 -0700

    initial commit

---
+++ PracticeLinq/PracticeLinq/Method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLinq
+{
+    public class Method
+    {
+        public static void PrintEnumerable(IEnumerable<string> enumerable)
+        {
+            foreach(string item in enumerable)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ PracticeLinq/PracticeLinq/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using PracticeLinq;
+
+
+List<string> gameNames = new List<string>()
+{"God of war",
+"Runescape",
+"MultiVersus",
+"Roblox",
+"Gundam Evolution"};
+
+IEnumerable<string> sorted = gameNames.OrderBy(game => game.Length);
+Method.PrintEnumerable(sorted);
